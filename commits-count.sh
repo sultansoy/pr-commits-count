35f9e65166e22b62e7a8217b3bab0bc3e09cde80
Rustam Sultansoy <rustam.sultansoy@gmail.com>
2025-03-24 00:04:15 +0300

    Fix commits-count.sh

--- commits-count.sh
+++ commits-count.sh
@@ -4,9 +4,6 @@ set -eou pipefail
 source=$1
 target=$2
 
-sourcePath=$(git branch -r | grep "$source" | xargs)
-targetPath=$(git branch -r | grep "$target" | xargs)
-
-commitsCount=$(git log --oneline "$sourcePath" \^"$targetPath" | wc -l)
+commitsCount=$(git log --oneline $source ^$target | wc -l)
 
 echo "$commitsCount"
